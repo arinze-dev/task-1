0e3ea2524838b8a10d6769d1d4eb28f40f927693
Anthony_dev <atozbasic@gmail.com>
2022-10-08 12:16:52 +0100

    update applied real use of react

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,33 +1,76 @@
 
-import React ,{useState} from 'react'
-import Data from './countries.json'
+import React ,{useState, useEffect} from 'react'
+import CountriesData from './countries.json'
 
 export function Countries() {
+  const [Countries,setCountries] = useState([]) 
+  const [SelectedCountries,setSelectedCountries ] = useState("")
+  const [State,setState] = useState([])
 
-  const [Countr, setCountr] = useState()
+  useEffect(()=>{
+    if (!CountriesData) return  
+    setCountries(Object.keys(CountriesData))
+  },[])
 
-  let countries = Object.keys(Data)
+  useEffect(()=>{
+    if (!SelectedCountries) return
+    let state = CountriesData[SelectedCountries]
+    setState(state)
 
-  let countriesIndex = countries.indexOf(Countr)
+  },[SelectedCountries])
 
-  let state = Object.values(Data)
-
-  let stateValue = state[countriesIndex]
-
-
-
-  return (
+  return(
     <div className='listOfCountr'>
-     <select name="" id="firstSelect"onChange={(e)=> setCountr(e.target.value)} >
-      <option value="">select countries</option> 
-          {countries.map((item,i)=>(<option value={item} key={i}>{item}</option>  ))}
-      </select>
-
-      <select name="" id="secondSelect">
-      <option >select state</option> 
-      {Countr && stateValue.map((item,i)=>(<option value={item} key={i}>{item}</option>  ))}
-      </select>
-    </div>
+          <select name="" id="firstSelect"onChange={(e)=> setSelectedCountries(e.target.value)} >
+          <option value="">select countries</option> 
+          {Countries && Countries.map((item,i)=>(<option value={item} key={i}>{item}</option>  ))}
+          </select>
+           <select name="" id="secondSelect">
+           <option >select state</option> 
+             {State && State.map((item,i)=>(<option value={item} key={i}>{item}</option>))}
+         
+         </select>
+       </div>
   )
+
 }
+
+
+
+
+
+
+// export function Countries() {
+//   const [Countr, setCountr] = useState()
+//   const [stateV, setstateV] = useState()
+
+//   let countries = Object.keys(Data)
+//   let countriesIndex = countries.indexOf(Countr)
+//   let state = Object.values(Data) 
+
+//   useEffect(()=>{      
+//          setstateV(state[countriesIndex])
+//           console.log(stateV);
+//   },[Countr]);
+
+//   return (
+//     <div className='listOfCountr'>
+//      <select name="" id="firstSelect"onChange={(e)=> setCountr(e.target.value)} >
+//       <option value="">select countries</option> 
+//           {countries.map((item,i)=>(<option value={item} key={i}>{item}</option>  ))}
+//       </select>
+//       <select name="" id="secondSelect">
+//       <option >select state</option> 
+//         {stateV && stateV.map((item,i)=>(<option value={item} key={i}>{item}</option>))}
+     
+//       </select>
+//     </div>
+//   )
+// }
+
+  
+
+
+
+
 
